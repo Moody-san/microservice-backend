a22ef9286d5c867c2600e5fb9e9c98e57f6e3805
Moody-san <87976665+Moody-san@users.noreply.github.com>
2023-11-14 22:21:21 +0500

    Update app.go

--- app2/app.go
+++ app2/app.go
@@ -23,7 +23,7 @@ func main() {
 		}
 	})
 
-	log.Println("Starting app2 on :8080")
+	log.Println("This is app2 listenting on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatalf("Error occurred: %v", err)
 	}
