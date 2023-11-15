9a6ef80255f00c5a4ba187f48b1caf7848048c34
Moody-san <arshadhassan420@gmail.com>
2023-11-15 16:07:32 +0500

    added app3

--- app3/app.go
+++ app3/app.go
@@ -23,7 +23,7 @@ func main() {
 		}
 	})
 
-	log.Println("hello from app2 : 8080")
+	log.Println("hello from app3 : 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatalf("Error occurred: %v", err)
 	}
---
+++ app3/go.mod
@@ -0,0 +1,3 @@
+module github.com/Moody-san/microservice-backend/app3
+
+go 1.21.4
