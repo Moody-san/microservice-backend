04762276426a6183dbaafe9847dd9bec4ad09aa8
Moody-san <87976665+Moody-san@users.noreply.github.com>
2023-11-17 17:36:16 +0500

    Update app.go

--- app.go
+++ app.go
@@ -13,7 +13,7 @@ type Response struct {
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		res := Response{
-			Message: "Hello from application2",
+			Message: "Hello from application 2",
 		}
 
 		w.Header().Set("Content-Type", "application/json")
