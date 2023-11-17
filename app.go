df7d788303be430b4ee81303eee4ae884dcc7d0c
Moody-san <87976665+Moody-san@users.noreply.github.com>
2023-11-17 18:10:19 +0500

    Update app.go

--- app.go
+++ app.go
@@ -13,7 +13,7 @@ type Response struct {
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		res := Response{
-			Message: "Hello this is application 1 from Oracle CLoud K8s cluster from pod -> $hostname",
+			Message: "Hello this is application 1 from Oracle CLoud K8s cluster from pod -> ${Hostname}",
 		}
 
 		w.Header().Set("Content-Type", "application/json")
