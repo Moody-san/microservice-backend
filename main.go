648bf0ac3ea0b4553b83b13a5b67ce2cc4d03f80
Muhammad Ahmed <ahmed.jamil7410@gmail.com>
2024-03-17 01:54:07 +0500

    fixed variable error

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"errors"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -18,17 +19,32 @@ import (
 )
 
 func main() {
-	dsn := "user:password@tcp(localhost:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	log.Printf("dbUser: %v", dbUser)
+	log.Printf("dbName: %v", dbName)
+	log.Printf("dbPassword: %v", dbPassword)
+	log.Printf("dbHost: %v", dbHost)
+	log.Printf("dbPort: %v", dbPort)
+
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPassword, dbHost, dbPort, dbName)
+	db, err := gorm.Open(mysql.Open(connectionString), &gorm.Config{})
+
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
 
+	rabbitmqConnectionString := os.Getenv("RABBITMQ_URL")
+	log.Printf("RABBIT_MQURL: %v", rabbitmqConnectionString)
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
 	orderService := service.NewOrderService(db)
-
 	r := mux.NewRouter()
 	api.RegisterOrderRoutes(r, orderService)
-
 	http.Handle("/", r)
 	httpServer := &http.Server{
 		Addr:    ":9020",
@@ -51,6 +67,6 @@ func main() {
 	if err := httpServer.Shutdown(ctx); err != nil {
 		log.Fatalf("HTTP server Shutdown failed: %v", err)
 	}
-	log.Println("Product service shutdown gracefully")
+	log.Println("Order service shutdown gracefully")
 
 }
